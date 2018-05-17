d05cbd4395317dc07dc69491f0e1b16785cdb747
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-05-18 00:11:29 +0100

    Updated design and added photos

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,20 @@ $(document).ready(() => {
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
-		adaptiveHeight: true,
+		//adaptiveHeight: true,
 		autoplay: true,
 		autoplaySpeed: 5000,
+	});
+
+
+	$('a[href="#menu"]').click((event) => {
+		event.preventDefault();
+		$('.mobile-menu').toggleClass('is-active');
+		$('#overlay').toggleClass('is-active');
+	});
+
+	$('#overlay').click((event) => {
+		$('.mobile-menu').toggleClass('is-active');
+		$('#overlay').toggleClass('is-active');
 	});
 });
